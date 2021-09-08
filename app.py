c480a17c448f6af5c5e9ea61dff446edf4f2bab2
Jônathas Nunes <nunes.jonathas@gmail.com>
2021-09-08 17:33:24 -0300

    Uploading Files

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
-
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify, render_template, request
 from zeep import Client
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
 
-@app.route('/consultar-cep', methods=['POST'])
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/consultar-cep", methods=["POST"])
 def consultar_cep():
-    client = Client('https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl')
-    data = client.service.consultaCEP(request.form.get('cep'))
+    client = Client(
+        "https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl"
+    )
+    data = client.service.consultaCEP(request.form.get("vapNuEtiquetaEncomenda"))
     data = data.__dict__
-    
-    return jsonify(data['__values__'])
+
+    return jsonify(data["__values__"])
+
 
 @app.errorhandler(404)
 def page_not_found(err):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(err):
-    return render_template('500.html'), 500
+    return render_template("500.html"), 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
-
